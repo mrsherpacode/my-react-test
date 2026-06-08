5174a82da80de185d393a33a65fb6ba7c76ec3b0
mrsherpacode <sherpacoding@gmail.com>
2026-06-08 14:25:42 -0500

    added more test to test input elements value and chage it when clicked

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -1,59 +1,31 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import AddInput from "../AddInput"
+import { fireEvent, render, screen } from "@testing-library/react";
+jest.mock("uuid", () => ({
+  v4: () => "test-id",
+}));
 
-const mockedSetTodo = jest.fn();
+import AddInput from "../AddInput";
+// mocking funtion
+const mockindTodos = jest.fn();
 
 describe("AddInput", () => {
-    it('should render input element', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        expect(inputElement).toBeInTheDocument();
-    });
-    
-    it('should be able to type into input', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        fireEvent.click(inputElement)
-        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } })
-        expect(inputElement.value).toBe("Go Grocery Shopping");
-    });
-    
-    it('should be able to type into input', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        fireEvent.click(inputElement)
-        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } });
-        const buttonElement = screen.getByRole("button", { name: /Add/i});
-        fireEvent.click(buttonElement)
-        expect(mockedSetTodo).toBeCalled()
-    });
-    
-    it('should have empty input when add button is cliked', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } });
-        const buttonElement = screen.getByRole("button", { name: /Add/i});
-        fireEvent.click(buttonElement)
-        expect(inputElement.value).toBe("")
-    });
-})
+  it("should render the input element", () => {
+    render(<AddInput todos={[]} setTodos={mockindTodos} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should be able to change the input element value", () => {
+    render(<AddInput todos={[]} setTodos={mockindTodos} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    fireEvent.change(inputElement, { target: { value: "learn react" } });
+    expect(inputElement.value).toBe("learn react");
+  });
+
+  it("should empty an input element's value once the button is clicked", () => {
+    render(<AddInput todos={[]} setTodos={mockindTodos} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(inputElement.value).toBe("");
+  });
+});
